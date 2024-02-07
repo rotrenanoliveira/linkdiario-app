7006a127476326de7aa9c2617dfb95b1095d93bd
Renan Oliveira <rotrenanoliveira@gmail.com>
2024-02-07 10:06:00 -0300

    feat: add button to register campaign on /dashboard/campaigns

--- src/app/(app)/dashboard/campaigns/page.tsx
+++ src/app/(app)/dashboard/campaigns/page.tsx
@@ -1,3 +1,25 @@
+import Link from 'next/link'
+import { Plus } from 'lucide-react'
+
+import { buttonVariants } from '@/components/ui/button'
+import { Separator } from '@/components/ui/separator'
+import { cn } from '@/lib/utils'
+
 export default function CampaignsPage() {
-  return <div>Campaigns</div>
+  return (
+    <section>
+      <div className="flex items-center justify-between">
+        <h2 className="text-2xl">Campanhas</h2>
+
+        <Link
+          className={cn(buttonVariants({ variant: 'outline' }), 'flex items-center gap-2')}
+          href={'/dashboard/campaigns/register'}
+        >
+          Nova campanha
+          <Plus strokeWidth={1} />
+        </Link>
+      </div>
+      <Separator className="my-4" />
+    </section>
+  )
 }
