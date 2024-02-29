38ac8356c95dbca6ec4e230c062b17d808bd9c79
Pedro Mendes <pedrohenriquecamposmendes@hotmail.com>
2024-02-29 16:54:31 -0300

    feat: create input value handlers and get info from the client in leads campaign

--- src/app/(client)/_components/leads-campaign.tsx
+++ src/app/(client)/_components/leads-campaign.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import * as React from 'react'
 import { satochi } from '@/app/fonts'
 import { Button } from '@/components/ui/button'
 import { FormItem } from '@/components/ui/form'
@@ -8,6 +11,9 @@ import { cn } from '@/lib/utils'
 import { Translate } from '@/utils/translate'
 import { MoveUpRight } from 'lucide-react'
 import Link from 'next/link'
+import { useFormState } from 'react-dom'
+import { saveLeadsValues } from '../preview/[id]/actions'
+import { useToast } from '@/components/ui/use-toast'
 
 interface LeadsCampaignProps {
   leadsInputs: CampaignLeads
@@ -16,36 +22,79 @@ interface LeadsCampaignProps {
 
 export function LeadsCampaign({ leadsInputs, affiliateUrl }: LeadsCampaignProps) {
   const activeInputs = leadsInputs.inputs.filter((input) => input.isActive)
+
+  const ref = React.useRef<HTMLFormElement>(null)
+
+  const [formState, formAction] = useFormState(saveLeadsValues, null)
+  const [inputValue, setInputValue] = React.useState<{ [key: string]: string }>({})
+
+  function handleValueChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = e.target
+
+    setInputValue((prev) => {
+      return { ...prev, [name]: value }
+    })
+  }
+
+  const { toast } = useToast()
+
+  React.useEffect(() => {
+    if (formState) {
+      toast({
+        variant: formState.success ? 'default' : 'destructive',
+        title: formState.title,
+        description: formState.message,
+        duration: 3000,
+      })
+
+      if (formState.success === false) {
+        return
+      }
+
+      ref.current?.reset()
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [formState])
+
+  const inputValueString = JSON.stringify(inputValue)
+
+  console.log(inputValueString)
   return (
     <div className="w-full max-w-[384px] space-y-4">
       <h1>Para receber atualizações sobre o produto, preencha os campos abaixo</h1>
-      {activeInputs.map((input) => {
-        return (
-          <FormItem key={input.name}>
-            <Label className="w-full">{Translate.leadName(input.name)}</Label>
 
-            <Input
-              type="text"
-              id="campaign-lead-name"
-              name="campaign-lead-name"
-              placeholder="Preencha o campo com seus dados."
-              required
-            />
-          </FormItem>
-        )
-      })}
+      <form ref={ref} action={formAction} className="w-full space-y-4">
+        {activeInputs.map((input) => {
+          return (
+            <FormItem key={input.name}>
+              <Label className="w-full">{Translate.leadName(input.name)}</Label>
 
-      <Button className="w-full h-16 rounded-full p-1 bg-[#EAEBEC] hover:bg-[#DDDEDF]">
-        <Link
-          href={affiliateUrl}
-          className={cn(satochi.className, 'w-full flex items-center justify-evenly text-3xl text-foreground')}
-        >
-          <span className="flex-grow text-center font-normal">Saiba mais</span>
-          <div className="size-14 rounded-full bg-yellow-400 flex items-center justify-center">
-            <MoveUpRight size={32} className="text-foreground/85" />
-          </div>
-        </Link>
-      </Button>
+              <Input
+                type="text"
+                id="campaign-lead-name"
+                name={input.name}
+                placeholder="Preencha o campo com seus dados."
+                required
+                onChange={handleValueChange}
+              />
+            </FormItem>
+          )
+        })}
+
+        <Input type="text" name="campaign-leads-inputs" readOnly value={inputValueString} className="hidden" />
+
+        <Button className="w-full h-16 rounded-full p-1 bg-[#EAEBEC] hover:bg-[#DDDEDF]">
+          <Link
+            href={affiliateUrl}
+            className={cn(satochi.className, 'w-full flex items-center justify-evenly text-3xl text-foreground')}
+          >
+            <span className="flex-grow text-center font-normal">Saiba mais</span>
+            <div className="size-14 rounded-full bg-yellow-400 flex items-center justify-center">
+              <MoveUpRight size={32} className="text-foreground/85" />
+            </div>
+          </Link>
+        </Button>
+      </form>
     </div>
   )
 }
--- src/app/(client)/preview/[id]/actions.ts
+++ src/app/(client)/preview/[id]/actions.ts
@@ -1,9 +1,13 @@
 'use server'
 
+import { randomUUID } from 'node:crypto'
 import { revalidatePath } from 'next/cache'
+import { fromZodError } from 'zod-validation-error'
+import { z } from 'zod'
 
 import { RedisCacheRepository } from '@/infra/cache/redis-cache-repository'
 import { CampaignsRepository } from '@/infra/database/db'
+import { ActionResponse } from '@/core/types'
 
 export async function publishCampaign(campaignId: string) {
   const campaignSlugs = await CampaignsRepository.getCompanyAndCampaignSlugById(campaignId)
@@ -23,4 +27,43 @@ export async function publishCampaign(campaignId: string) {
   revalidatePath(`/dashboard/campaigns/`)
   revalidatePath(`/preview/${campaignId}/`)
   revalidatePath(`/${campaignSlugs.companySlug}/${campaignSlugs.campaignSlug}`)
+}
+
+type PrevState = ActionResponse | null
+
+export async function saveLeadsValues(prevState: PrevState, data: FormData): Promise<ActionResponse> {
+  const saveLeadsSchema = z.object({
+    campaignId: z.string().uuid({ message: 'ID inválido.' }),
+    leads: z.string(),
+  })
+
+  const result = saveLeadsSchema.safeParse({
+    campaignId: data.get('campaign-id'),
+    leads: data.get('campaign-leads-inputs'),
+  })
+
+  if (result.success === false) {
+    const zodError = fromZodError(result.error)
+
+    return {
+      success: false,
+      title: 'Algo deu errado!',
+      message: zodError.toString(),
+    }
+  }
+
+  const leads = {
+    id: randomUUID(),
+    ...result.data,
+  }
+
+  return leads
+  // TODO create the Leads Repository
+  /*
+  await LeadsRepository.create({
+    id: leads.id,
+    campaignId: leads.campaignId,
+    leads: leads.leads,
+  })
+  */
 }
