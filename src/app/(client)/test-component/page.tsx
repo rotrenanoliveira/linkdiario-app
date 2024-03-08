839117c694df366641696e5fe39898495a24f5c8
Renan Oliveira <rotrenanoliveira@gmail.com>
2024-03-08 20:29:27 -0300

    fix: add item on top of footer
    adding item on top just to better visualize the component

--- src/app/(client)/test-component/page.tsx
+++ src/app/(client)/test-component/page.tsx
@@ -2,7 +2,8 @@ import { Footer } from '../_components/footer'
 
 export default function HomePage() {
   return (
-    <div className="w-screen min-h-svh flex items-center justify-center p-3 md:p-9 lg:p-12 xl:p-16">
+    <div className="w-screen min-h-svh flex flex-col items-center justify-center p-3 md:p-9 lg:p-12 xl:p-16">
+      <div className="w-full min-h-20" />
       <Footer />
     </div>
   )
