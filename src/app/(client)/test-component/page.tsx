cb0bb07545fd2e9229ef0a2f17cac9d125cc68a7
Renan Oliveira <rotrenanoliveira@gmail.com>
2024-03-11 12:23:49 -0300

    feat: add Q&A section

--- src/app/(client)/test-component/page.tsx
+++ src/app/(client)/test-component/page.tsx
@@ -1,8 +1,10 @@
+import { QuestionsAndAnswers } from '../_components/ qna'
 import { Footer } from '../_components/footer'
 
 export default function HomePage() {
   return (
     <div className="w-screen min-h-svh flex flex-col items-center justify-center p-3 md:p-9 lg:p-12 xl:p-16">
+      <QuestionsAndAnswers />
       <Footer />
     </div>
   )
